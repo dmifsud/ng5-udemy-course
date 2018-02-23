17958a78f913b33eb21ac30278ca176bb2ee1197
dmifsud <david@Davids-MBP.lan>
2018-02-23 15:26:24 +0100

    Add services

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { RecipeModel } from '../recipe.model';
+import { RecipeService } from '../../shared/services/recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
+  providers: [RecipeService]
 })
 export class RecipeListComponent implements OnInit {
   @Output() onSelectedRecipe = new EventEmitter<RecipeModel>();
-  recipes: RecipeModel[] = [
-    new RecipeModel('Burger', 'Whole food burger', 'http://assets.wholefoodsmarket.com/recipes/2762/460/290/2762.jpg'),
-    new RecipeModel('Another Burger', 'Another Whole food burger',
-                    'https://www.peta.org/wp-content/uploads/2015/05/eggplant-vegan-burger-602x585-1432838012.jpg')
-  ];
-  constructor() { }
+  recipes: RecipeModel[];
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.recipes;
   }
 
   onSelectedRecipeItem(recipe: RecipeModel) {
---
+++ src/app/shared/services/recipe.service.ts
@@ -0,0 +1,8 @@
+import { RecipeModel } from '../../recipes/recipe.model';
+export class RecipeService {
+    recipes: RecipeModel[] = [
+        new RecipeModel('Burger', 'Whole food burger', 'http://assets.wholefoodsmarket.com/recipes/2762/460/290/2762.jpg'),
+        new RecipeModel('Another Burger', 'Another Whole food burger',
+                        'https://www.peta.org/wp-content/uploads/2015/05/eggplant-vegan-burger-602x585-1432838012.jpg')
+      ];
+}
